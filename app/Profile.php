e5174bc67e4317149d5f014daef8fffcb67cc2e2
f3rnand0_lp@hotmail.com <f3rnand0_lp@hotmail.com>
2019-08-25 13:57:28 -0300

    Default Image

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $guarded = [];
+
+    public function profileImage(){
+        return ($this->image) ? '/storage/'. $this->image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png'; 
+    }
+
+
     public function user(){
         return $this->belongsTo(User::class);
     }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,14 +4,23 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="http://pluspng.com/img-png/instagram-png-instagram-png-logo-1179.png" alt="" class="rounded-circle w-100">
+            <img src="{{ $user->profile->profileImage() }}" alt="" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
 
-                <h1>{{ $user->username}}</h1>
-                <a href="/p/create">Add new post</a>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{ $user->username}}</div>
+                    <button class="btn btn-primary ml-4">Follow</button>  
+                </div> 
+
+                @can('update', $user->profile)
+                    <a href="/p/create">Add new post</a>
+                @endcan
             </div>
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
