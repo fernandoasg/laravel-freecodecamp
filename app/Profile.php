8713f6cccb354f8e681eef1bcead8c80815eb68e
f3rnand0_lp@hotmail.com <f3rnand0_lp@hotmail.com>
2019-08-25 14:39:55 -0300

    Follow and Unfollow

--- app/Profile.php
+++ app/Profile.php
@@ -12,6 +12,9 @@ class Profile extends Model
         return ($this->image) ? '/storage/'. $this->image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png'; 
     }
 
+    public function followers(){
+        return $this->belongsToMany(User::class);
+    }
 
     public function user(){
         return $this->belongsTo(User::class);
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,7 +11,7 @@
 
                 <div class="d-flex align-items-center pb-3">
                     <div class="h4">{{ $user->username}}</div>
-                    <button class="btn btn-primary ml-4">Follow</button>  
+                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>  
                 </div> 
 
                 @can('update', $user->profile)
@@ -23,8 +23,8 @@
             @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
-                <div class="pr-5"><strong>23k</strong> followers</div>
-                <div class="pr-5"><strong>212</strong> following</div>
+                <div class="pr-5"><strong>{{ $user->profile->followers->count() }}</strong> followers</div>
+                <div class="pr-5"><strong>{{ $user->following->count() }}</strong> following</div>
             </div>
         <div class="pt-4"><strong>{{ $user->profile->title }}</strong></div>
             <div>{{ $user->profile->description }}</div>
